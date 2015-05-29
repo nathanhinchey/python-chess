f14848e41c5e225813ce69e5a09d907b2cf5f6aa
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-28 23:35:27 -0400

    Finish Piece class

--- pieces/piece.py
+++ pieces/piece.py
@@ -3,3 +3,19 @@ class Piece:
         self.color = options['color']
         self.board = options['board']
         self.pos = options['pos']
+
+        board.add_piece(self, pos)
+
+    def render():
+        symbols[color]
+
+    def valid_moves():
+        _valid_moves = []
+        for to_pos in moves():
+            if !move_into_check(to_pos):
+                _valid_moves.push(to_pos)
+
+    def move_into_check():
+        test_board = board.dup()
+        test_board.move_piece(pos, to_pos)
+        test_board.in_check?(self.color)
